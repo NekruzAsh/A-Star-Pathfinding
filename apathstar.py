42d238df8e82f0d87cca0b53a41841012051f40f
Nekruz Ashrapov <64292629+NekruzAsh@users.noreply.github.com>
2024-04-08 21:42:14 -0400

    first commit

---
+++ apathstar.py
@@ -0,0 +1,27 @@
+
+# author: Nekruz Ashrapov
+# date: 4/8/2024 
+
+import pygame
+import math
+from queue import PriorityQueue
+
+
+
+WIDTH = 900
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A path star algorithm")
+
+RED = (255, 0, 0)           #constant values don't change
+BLUE = (0, 255, 0)
+GREEN = (0, 0, 255)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
+BLACK = (0, 0, 0)
+ORANGE = (255, 165, 0)
+WHITE = (255, 255, 255)
+
+
+
+
+    
